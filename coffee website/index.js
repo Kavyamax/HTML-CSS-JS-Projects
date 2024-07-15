cf383283cbe1829e2f4ede0d11c122cad01e2ad8
Kavya <Kavyaishu003@gmail.com>
2024-07-15 13:45:35 +0530

    initial commit

---
+++ coffee website/index.js
@@ -0,0 +1,12 @@
+// document.write("Hello World!");
+// document.write("Kavya loves ice cream");
+// console.log("Hello, World");
+// console.log("I love Cakes!!!")
+const a = 10
+const b = 30
+let result = a+b;
+console.log("a : "+a);
+console.log("b : "+b);
+console.log("sum : "+result);
+
+
